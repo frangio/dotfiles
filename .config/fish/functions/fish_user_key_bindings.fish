4ce5eb3703df4590699ee36863c642ec8b775ec3
Francisco Giordano <frangio.1@gmail.com>
2020-01-01 19:51:48 -0300

    do not run fzf_key_bindings unless present

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,8 @@
 # Defined in /tmp/fish.ww5pOt/fish_user_key_bindings.fish @ line 2
 function fish_user_key_bindings
-	fzf_key_bindings
+	if functions -q fzf_key_bindings
+		fzf_key_bindings
+	end
 	bind \en nvim
         bind \eF forward-bigword
 end
